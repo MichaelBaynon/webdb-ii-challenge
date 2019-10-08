1950d42940aae849845666126ec4e679792c46d9
MichaelBaynon <baynonmichael@gmail.com>
2019-10-08 18:09:42 -0400

    mvp

---
+++ index.js
@@ -0,0 +1,6 @@
+// npm install express, helmet, and sqlite3
+
+const server = require('./api/server')
+
+const port = process.env.PORT || 5000
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`))
